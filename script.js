d0e88aef1d19c4e03a72fe485692058271292760
mattrich98 <matthew_richardson@ymail.com>
2024-01-02 01:51:04 +0000

    function tests for gameloop

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+let gameTime = 0;
+let gameScore = 0;
+let gameLoop = 0;
 
 console.log(Math.floor(Math.random() * 81));
 
-function movePlayers() {
-    console.log("movePlayers test")
-    return "players moved places"
-}
 
 function startGame() {
     console.log("test");
+    gameIntervals();
     movePlayers();
     return "game started";
+}
+
+function gameIntervals(){
+    if(gameLoop <= 19){
+        gameLoop++
+        }
+        else if (gameloop >= 19){
+        gameLoop= false;
+    }
+    console.log(gameLoop);
+    return "Game Over";
+}
+
+function movePlayers() {
+    console.log("movePlayers test")
+
+    return "players moved places"
 }
