df186b1590d610ff8773ab9378beda6a4703bef1
mattrich98 <matthew_richardson@ymail.com>
2024-01-02 21:25:16 +0000

    added game over alert + score, back

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function gameIntervals(){
     }
 
     else if (gameTime >= 5){
+        alert("Game over! you scored: " + gameScore);
         console.log("Game Over");
         document.getElementById("startButton").innerHTML = "Play again?";
         }
