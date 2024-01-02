457b17faf9a2e3ac29794827f6b4ae25906e2de6
mattrich98 <matthew_richardson@ymail.com>
2024-01-02 00:03:22 +0000

    deleted everything and tested basic function linked with button

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-let enemy;
-let person;
-let gameBoard;
-let playButton;
-
-const timer = 0;
-const points = 0;
-
-let gameStart = false;
-let gameLoop;
-let gameEnd = false;
-
-
 function startGame() {
-  if (gameStart = true) {
-    return "game has started";
-  }
-  else if (gameStart = false)  {
-    return "game not started";
-  }
+    console.log("test");
 }
 
-startGame();
-
-console.log (startGame());
+test()
+test()
+test()
