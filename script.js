fbfc1b24baeaa657a3af75d5ebc9cc19866bd351
mattrich98 <matthew_richardson@ymail.com>
2024-01-02 02:21:11 +0000

    game for loop  added for time

--- script.js
+++ script.js
@@ -7,25 +7,29 @@ console.log(Math.floor(Math.random() * 81));
 
 
 function startGame() {
-    console.log("test");
     gameIntervals();
     movePlayers();
     return "game started";
 }
 
 function gameIntervals(){
-    if(gameLoop <= 59){
-        gameLoop++
-        }
-        else if (gameLoop == 60){
-        gameLoop= 60;
+    for (let i = 0; i <= 60; i++){
+        console.log(i);
     }
+
     console.log(gameLoop);
     return "Game Over";
 }
 
 function movePlayers() {
-    console.log("movePlayers test")
-    
     return "players moved places"
 }
+
+
+
+//if(gameLoop == 60){
+//    gameLoop == 60
+//  }
+//  else if (gameLoop == 60){
+//      gameLoop= 60;
+//}
