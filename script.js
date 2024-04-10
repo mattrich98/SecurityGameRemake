05e3fbb78334ace4fb4ce207744b6786ca0d15d8
Matthew Richardson <matthew_richardson@ymail.com>
2024-04-10 18:31:38 +0100

    plan to randomise enemy and player locations

--- script.js
+++ script.js
@@ -46,9 +46,18 @@ function pointsSystem() {
 
 function movePlayers() {
 
-  
+
 }
 
+
+/* 
+  To select the HTML Divs in CSS, display: none -> display: block;
+
+  To randomise -> Create a section of several enemy/person blocks that disappear and reappear using math.random
+  --Children of a section must be parents. Cons - Predictability makes the game easier.
+  ---But this is how I can come up with the solution with my current skills.
+
+Use a for loop. 
 
 /*
 
@@ -63,6 +72,8 @@ childArray.sort(() => Math.random() - 0.5);
     document.querySelector("#Board").innerHTML = childArray;
     childArray = parentBlock;
     console.log(childArray);
+
+    This will not work.
 */
 
 /*
@@ -77,27 +88,4 @@ childArray.sort(() => Math.random() - 0.5);
     boardArray.sort(() => Math.random() - 0.5);
     document.getElementById("Board").childElementCount =  boardArray;
     console.log(boardArray);
-/*
-
-//console.log(divChildren);
-//
-/*const players = [];
-players.sort/*
-
-//document.getElementById("startButton").innerHTML = "Click below to play again";
-
-/*function refreshPage() {
-    console.log("refreshed page");
-    location.reload();
-}/*
-
-//if(gameLoop == 60){ --- cant slow down loop so needs to be if/else
-//    gameLoop == 60
-//  }
-//  else if (gameLoop == 60){
-//      gameLoop= 60;
-//}
-
-/*
-    for (let i = 0; i <= 60; i ++ ){
 */
