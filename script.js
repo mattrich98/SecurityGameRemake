cb9fff8ca4475062061e2c1f6b62097fcd080fcf
Matthew Richardson <matthew_richardson@ymail.com>
2024-04-22 11:40:10 +0100

    comments to understand update

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let gameTime = 0; //number for Time that will increment
-let gameScore = 0; //number for Score that will increment
+let gameTime = 0; //number for time that will increment as the game plays out.
+let gameScore = 0; //number for the player's score that will increment.
 
-function startGame() { //starts Game with nested functions
-  gameIntervals(); //nested function for game changes that take place every second
-  pointsSystem(); //nested function to gain or lose points
-  movePlayers(); //nested function to move the players
+function startGame() { //starts the game with nested functions
+  gameIntervals(); //games timer function
+  pointsSystem(); //gain or lose points function
+  movePlayers(); //moving players function 
 }
 
-function gameIntervals() {
-  gameTime++;
-  if (gameTime <= 20) {
+function gameIntervals() { 
+  gameTime++; //gametime gets incremented +1 every second
+  if (gameTime <= 20) { //if the game time is less than 20 seconds
 
     //Loops gameIntervals, 750 = time between
-    setTimeout(gameIntervals, 750);
+    setTimeout(gameIntervals, 750); //method to slow down the game timer.
 
     //link with HTML timer
     document.getElementById("timeNumber").innerHTML = gameTime; 
