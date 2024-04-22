d96898d498c7c3b9d5f39c65ba4af9412e84ea72
Matthew Richardson <matthew_richardson@ymail.com>
2024-04-22 11:44:36 +0100

    comments update 2

--- script.js
+++ script.js
@@ -9,22 +9,22 @@ function startGame() { //starts the game with nested functions
 
 function gameIntervals() { 
   gameTime++; //gametime gets incremented +1 every second
-  if (gameTime <= 20) { //if the game time is less than 20 seconds
+  if (gameTime <= 20) { //if the game time is less than 20 seconds, below will occur
 
     //Loops gameIntervals, 750 = time between
     setTimeout(gameIntervals, 750); //method to slow down the game timer.
 
     //link with HTML timer
-    document.getElementById("timeNumber").innerHTML = gameTime; 
-    document.getElementById("startButton").innerHTML = "Game Started";
+    document.getElementById("timeNumber").innerHTML = gameTime; //links the gameTime variable with HTML timer div
+    document.getElementById("startButton").innerHTML = "Game Started"; //changes the start button inner letters
 
     //stops play button press
-    document.getElementById("startButton").onclick = false; 
+    document.getElementById("startButton").onclick = false; //so you can only click the play button once to the start game
 
     //stops setTimeout loop
-  } else if (gameTime >= 20) {
-    document.getElementById("startButton").innerHTML = "Game Over";
-    alert("Game over! you scored: " + gameScore);
+  } else if (gameTime >= 20) { //if the timer reaches 20 seconds
+    document.getElementById("startButton").innerHTML = "Game Over"; //changes the start button inner letters
+    alert("Game over! you scored: " + gameScore); //method alert pop up in browser for final score
     //console.log("Game Over");
    
   }
